c0d3916cffc92be2bf50d01153a0c8402af2ab03
FeaturedEpic <lcao1756@gmail.com>
2015-07-15 21:44:52 +0100

    Create Rule.java

---
+++ Rule.java
@@ -0,0 +1,106 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+
+/**
+ * @author FeaturedEpic
+ *
+ */
+public class Rule {
+	
+	private String name;
+	private List<String> lore = new ArrayList<String>();
+	private Material item;
+	
+	
+	/**
+	 * Creates a new Rule with given name.
+	 * 
+	 * @param  name  The name of the Rule
+	 */
+	public Rule(String name){
+		this.name = name;
+	}
+	
+	/**
+	 * Gets the name of the Rule.
+	 * 
+	 * @return  The Name of the Rule.
+	 */
+	public String getName(){
+		return this.name;
+	}
+	
+	/**
+	 * Gets the description of the Rule. The
+	 * description is the lore of the item
+	 * that represents the Rule in-game.
+	 * 
+	 * @return  The Description of the Rule.
+	 */
+	public List<String> getDescription(){
+		return this.lore;
+	}
+	
+	/**
+	 * Gets the item used in-game to represent
+	 * the Rule in-game.
+	 * 
+	 * @return  The Item used to Represent the Rule.
+	 */
+	public Material getItem(){
+		return this.item;
+	}
+	
+	/**
+	 * Sets the name of the Rule.
+	 * 
+	 * @param  name  The Name.
+	 * @return       The Rule.
+	 */
+	public Rule setName(String name){
+		this.name = name;
+		return this;
+	}
+	
+	/**
+	 * Sets the description of the Rule. The
+	 * description is the lore of the item
+	 * that represents the Rule in-game.
+	 * 
+	 * @param  description  The Description.
+	 * @return              The Rule.
+	 */
+	public Rule setDescription(List<String> description){
+		this.lore = description;
+		return this;
+	}
+	
+	/**
+	 * Adds a new line to the description of
+	 * the Rule. The description is the lore of
+	 * the item that represents the Rule
+	 * in-game.
+	 * 
+	 * @param  description  The Line of the Description.
+	 * @return              The Rule.
+	 */
+	public Rule addToDescription(String description){
+		this.lore.add(description);
+		return this;
+	}
+	
+	/**
+	 * Sets the item of the Rule. The item is the
+	 * visual representation of the Rule in-game.
+	 * 
+	 * @param  item  The Item.
+	 * @return       The Rule.
+	 */
+	public Rule setItem(Material item){
+		this.item = item;
+		return this;
+	}
+
+}
